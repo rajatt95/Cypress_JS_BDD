5a0fb7074e98f97939259ddb9f66ce420559987d
“rajatt95” <rajatvermaa95@gmail.com>
2022-04-29 01:34:16 +0530

    Details added in files

--- cypress/integration/02-Github_CypressCucumberPreProcessor/DataTable/dataTable.js
+++ cypress/integration/02-Github_CypressCucumberPreProcessor/DataTable/dataTable.js
@@ -1,3 +1,15 @@
+/**  
+* @author Rajat Verma
+* https://www.linkedin.com/in/rajat-v-3b0685128/
+* https://github.com/rajatt95
+* https://rajatt95.github.io/
+* 
+* Course: Cypress -Modern Automation Testing from Scratch + Frameworks(https://www.udemy.com/course/cypress-tutorial/)
+* Tutor: Rahul Shetty (https://www.udemy.com/user/rahul445/)
+*/
+
+/***************************************************/
+
 // eslint-disable-next-line import/no-extraneous-dependencies
 const { Given, When, Then } = require("cypress-cucumber-preprocessor/steps");
 
--- cypress/integration/11-BDD_Google_NW/Google.feature
+++ cypress/integration/11-BDD_Google_NW/Google.feature
@@ -1,3 +1,15 @@
+# /**  
+# * @author Rajat Verma
+# * https://www.linkedin.com/in/rajat-v-3b0685128/
+# * https://github.com/rajatt95
+# * https://rajatt95.github.io/
+# * 
+# * Course: Cypress -Modern Automation Testing from Scratch + Frameworks(https://www.udemy.com/course/cypress-tutorial/)
+# * Tutor: Rahul Shetty (https://www.udemy.com/user/rahul445/)
+# */
+
+# /***************************************************/
+
 Feature: Google Main Page
 
   # node cucumber-html-report.js
